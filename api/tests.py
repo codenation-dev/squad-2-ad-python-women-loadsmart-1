77918d0c604d825fa32f9eca0ec4369ec742979c
Rodrigo Machado <rodrigo@machado.io>
2019-11-19 10:28:52 -0300

    Add test skeleton

--- api/tests.py
+++ api/tests.py
@@ -1,3 +1,7 @@
 from django.test import TestCase
+from rest_framework.test import APITestCase
 
 # Create your tests here.
+class TestSerializer(APITestCase):
+    def test_do_something(self):
+        self.assertTrue(False)
