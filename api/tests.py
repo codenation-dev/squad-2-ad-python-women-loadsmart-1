ac89c71f2db9d9dea7cdc7c7ccff5402563d4cc0
marcela <marcelafvieira@gmail.com>
2019-11-19 10:54:23 -0300

    assert

--- api/tests.py
+++ api/tests.py
@@ -2,6 +2,7 @@ from django.test import TestCase
 from rest_framework.test import APITestCase
 from api.models import Event, Agent, Environment
 from users.models import CustomUser
+from api.serializers import EventSerializer
 
 # Create your tests here.
 class TestSerializer(APITestCase):
@@ -11,5 +12,7 @@ class TestSerializer(APITestCase):
         test_user = CustomUser.objects.create(first_name="Marcela", last_name="Vieira", email="marcela@gmail.com", password="newpassword654")
         test_event = Event.objects.create(level="C", agent=test_agent, user=test_user)
 
-        address = get_address_from_agent(self, test_event)
+        event_serializer = EventSerializer()
+
+        address = event_serializer.get_address_from_agent(test_event)
         self.assertEqual(address, "255.255.255.255")
