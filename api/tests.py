13a60567e85c5112e212db64be7eb4ee0a4e0ce9
marcela <marcelafvieira@gmail.com>
2019-11-19 10:41:48 -0300

    exit enter the commit message for your changes. Lines starting

--- api/tests.py
+++ api/tests.py
@@ -1,7 +1,12 @@
 from django.test import TestCase
 from rest_framework.test import APITestCase
+from api.models import Event, Agent
 
 # Create your tests here.
 class TestSerializer(APITestCase):
     def test_do_something(self):
-        self.assertTrue(False)
+        test_agent = Agent.objects.create()
+        test_user = User.objects.create(first_name="Marcela", last_name="Vieira", email="marcela@gmail.com", password="newpassword654")
+        test_event = Event.objects.create(level="C", agent=test_agent, user=test_user)
+        # address =
+        self.assertTrue(True)
